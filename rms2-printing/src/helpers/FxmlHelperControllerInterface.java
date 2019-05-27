916e272de56ee1849c3f02779ae0a4443a30db63
minaammunir <minaammunir@gmail.com>
2019-05-27 16:39:12 +0500

    abc

---
+++ rms2-printing/src/helpers/FxmlHelperControllerInterface.java
@@ -0,0 +1,7 @@
+package helpers;
+
+public interface FxmlHelperControllerInterface {
+
+    void windowClose();
+
+}
---
+++ rms2-printing/src/module-info.java
@@ -0,0 +1,6 @@
+module rms2_printing {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens main;
+}
