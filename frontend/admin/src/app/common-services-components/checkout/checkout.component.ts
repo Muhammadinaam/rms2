39b7f805fc64ae71f13a0500bcfa1c2f4fa7fe6b
minaammunir <minaammunir@gmail.com>
2019-08-22 19:26:37 +0500

    a

--- frontend/admin/src/app/common-services-components/checkout/checkout.component.ts
+++ frontend/admin/src/app/common-services-components/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { OrderService } from '../services/order.service';
 import { Router } from '@angular/router';
+import { retry } from 'rxjs/operators';
 
 @Component({
   selector: 'checkout',
@@ -45,7 +46,7 @@ export class CheckoutComponent implements OnInit {
     }
 
     this.loading = true
-    this.orderService.saveOrder()
+    this.orderService.saveOrder().pipe(retry(5))
       .subscribe(data => {
         if(data['success'] == true)
         {
